bd899869d83dbe3a26e376d71a94a77fb48ec762
vova_lovalova <rudchenko.vova@mail.ru>
2023-01-18 00:21:41 +0800

    mobile v2
    меняется кнопка

--- src/script/toggle-button.js
+++ src/script/toggle-button.js
@@ -4,8 +4,12 @@ function onClickNavBarButton() {
     let navBar = document.querySelector(".nav-bar__list");
     if (navBar.classList.contains('nav-bar__list--closed')) {
         navBar.classList.remove('nav-bar__list--closed');
+        button.classList.remove('nav-bar__button--open');
+        button.classList.add('nav-bar__button--closed');
     } else {
         navBar.classList.add('nav-bar__list--closed');
+        button.classList.remove('nav-bar__button--closed');
+        button.classList.add('nav-bar__button--open');
     }
 }
 
