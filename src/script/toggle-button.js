106c1da7ad0be4425bfba1bcd97712ca42c1bb45
vova_lovalova <rudchenko.vova@mail.ru>
2023-01-18 11:56:36 +0800

    nav bar button finish

--- src/script/toggle-button.js
+++ src/script/toggle-button.js
@@ -1,16 +1,19 @@
 let button = document.querySelector(".nav-bar__button");
+let navBar = document.querySelector(".nav-bar__list");
 
-function onClickNavBarButton() {
-    let navBar = document.querySelector(".nav-bar__list");
-    if (navBar.classList.contains('nav-bar__list--closed')) {
-        navBar.classList.remove('nav-bar__list--closed');
-        button.classList.remove('nav-bar__button--open');
-        button.classList.add('nav-bar__button--closed');
-    } else {
+function onClickNavBarButton () {
+    navBar.classList.toggle('nav-bar__list--closed');
+    button.classList.toggle('nav-bar__button--open');
+    button.classList.toggle('nav-bar__button--closed');
+}
+
+function onClickDocument (e) {
+    if (e.target != button) {
         navBar.classList.add('nav-bar__list--closed');
-        button.classList.remove('nav-bar__button--closed');
         button.classList.add('nav-bar__button--open');
+        button.classList.remove('nav-bar__button--closed');
     }
 }
 
 button.addEventListener("click", onClickNavBarButton);
+document.addEventListener("click", onClickDocument);
